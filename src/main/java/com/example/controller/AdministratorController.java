0c862f17ebd1eb045f36a6d74d866b7afd526381
h-mizumura <h-mizumura@brexa.com>
2026-05-26 16:11:30 +0900

    岡本さんご指摘分修正済

--- src/main/java/com/example/controller/AdministratorController.java
+++ src/main/java/com/example/controller/AdministratorController.java
@@ -103,8 +103,11 @@ public class AdministratorController {
 	 * @return ログイン後の従業員一覧画面
 	 */
 	@PostMapping("/login")
-	public String login(LoginForm form, RedirectAttributes redirectAttributes, Model model) {
-
+	public String login(@Validated LoginForm form, BindingResult result, RedirectAttributes redirectAttributes, Model model) {
+		if(result.hasErrors()) {
+			return "login";
+		}
+		
 		Administrator administrator = administratorService.login(form.getMailAddress(), form.getPassword());
 		if (administrator == null) {
 			redirectAttributes.addFlashAttribute("errorMessage", "メールアドレスまたはパスワードが不正です。");
