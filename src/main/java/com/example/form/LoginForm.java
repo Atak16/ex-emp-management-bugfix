b1e9df9ab2d962f43eb5a5cd535830101ed8fc47
h-mizumura <h-mizumura@brexa.com>
2026-05-27 10:50:16 +0900

    bugfix/1-2-#4 入力値エラー処理

--- src/main/java/com/example/form/LoginForm.java
+++ src/main/java/com/example/form/LoginForm.java
@@ -17,15 +17,17 @@ public class LoginForm {
 	@Email(message="メールアドレスの形式が無効です。")
 	@NotBlank(message="メールアドレスを入力してください。")
 	@Size(min=1, max=256, message="256文字以内で入力してください。")
-	@Pattern(regexp = "^[ -~]+$", message="全角文字は入力できません。半角文字で入力してください。")
+	@Pattern(regexp = "^[a-zA-Z0-9]+$", message="全角文字は入力できません。半角文字で入力してください。")
 	@Pattern(regexp = "^[^\\s　]+$", message = "スペースを含めることはできません。")
+	@Pattern(regexp = "^[@_.-]+$", message = "使用できる記号は「@」「_」「.」「-」です。")
 	private String mailAddress;
 	/** パスワード */
 	@NotBlank(message="パスワードを入力してください。")
 	@Size(min=8, max=20, message="8文字以上20文字以内で入力してください。")
-	@Pattern(regexp = "^[ -~]+$", message="全角文字は入力できません。半角文字で入力してください。")
+	@Pattern(regexp = "^[a-zA-Z0-9]+$", message="全角文字は入力できません。半角文字で入力してください。")
 	@Pattern(regexp = "^[^\\s　]+$", message = "スペースを含めることはできません。")
 	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z]).+$", message="大文字、小文字をそれぞれ1文字以上含める必要があります。")
+	@Pattern(regexp = "^[!?@_.-]+$", message = "使用できる記号は「!」「?」「@」「_」「.」「-」です。")
 	private String password;
 
 	public String getMailAddress() {
